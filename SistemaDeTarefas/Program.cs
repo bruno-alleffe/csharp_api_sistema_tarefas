34d9b02974b79c1acab72399065b7448304e7bea
Bruno Alleffe <bruno16alleffe@gmail.com>
2023-04-20 20:15:51 -0300

    Criado conexão com banco e Enum de status da tarefa

--- SistemaDeTarefas/Program.cs
+++ SistemaDeTarefas/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Data;
+using SistemaDeTarefas.Repositorios;
+using SistemaDeTarefas.Repositorios.Interfaces;
+
 namespace SistemaDeTarefas {
     public class Program {
         public static void Main(string[] args) {
@@ -9,6 +14,13 @@ namespace SistemaDeTarefas {
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddEntityFrameworkSqlServer()
+                .AddDbContext<SistemaTarefasDBContext>(
+                    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DataBase"))
+                );
+
+            builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
 
             var app = builder.Build();
 
