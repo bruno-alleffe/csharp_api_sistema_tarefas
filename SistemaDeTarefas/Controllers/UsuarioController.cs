0098993b04cdea023feeb4ed8db35651032e4e51
Bruno Alleffe <bruno16alleffe@gmail.com>
2023-04-21 19:55:53 -0300

    Implementado Controller de Usuario

--- SistemaDeTarefas/Controllers/UsuarioController.cs
+++ SistemaDeTarefas/Controllers/UsuarioController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SistemaDeTarefas.Models;
+using SistemaDeTarefas.Repositorios;
 using SistemaDeTarefas.Repositorios.Interfaces;
+using System.Threading.Tasks.Dataflow;
 
 namespace SistemaDeTarefas.Controllers
 {
@@ -20,6 +22,35 @@ namespace SistemaDeTarefas.Controllers
         {
             List<UsuarioModel> usuarios = await _usuarioRepositorio.BuscarTodosUsuarios();
             return Ok(usuarios);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UsuarioModel>> BurcarPorId(int id)
+        {
+            UsuarioModel usuario = await _usuarioRepositorio.BuscaPorId(id);
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UsuarioModel>> Cadastrar([FromBody] UsuarioModel usuarioModel)
+        {
+            UsuarioModel usuario = await _usuarioRepositorio.Adicionar(usuarioModel);
+            return Ok(usuario);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<UsuarioModel>> Atualizar([FromBody] UsuarioModel usuarioModel, int id)
+        {
+            usuarioModel.Id = id;
+            UsuarioModel usuario = await _usuarioRepositorio.Atualizar(usuarioModel, id);
+            return Ok(usuario);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<UsuarioModel>> Apagar(int id)
+        {
+            bool apagado = await _usuarioRepositorio.Apagar(id);
+            return Ok(apagado);
         }
     }
 }
