9358f0bd612b0e198cbb43a7eef6a1ce8322f556
Bruno Alleffe <bruno16alleffe@gmail.com>
2023-04-22 18:38:22 -0300

    Adicionado Interface e Repositorio de Tarefa

--- SistemaDeTarefas/Models/TarefaModel.cs
+++ SistemaDeTarefas/Models/TarefaModel.cs
@@ -6,7 +6,7 @@ namespace SistemaDeTarefas.Models
     {
         public int Id { get; set; }
         public string? Nome { get; set; }
-        public string? Descricao { get;}
+        public string? Descricao { get; set;}
         public StatusTarefa Status { get; set; }
         public int? UsuarioId { get; set; }
         public virtual UsuarioModel? Usuario{ get; set; }
